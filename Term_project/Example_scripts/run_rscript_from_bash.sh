06f36d9d2929a2799a3234ebdddafe61eb92b030
limey-bean <eecurd@g.ucla.edu>
2019-05-06 12:24:09 -0700

    example

---
+++ Term_project/Example_scripts/run_rscript_from_bash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# this the the format for having bash run and r script.  It you hade command line arguments they would follow "test_r_script.R"
+Rscript --vanilla test_r_script.R
---
+++ Term_project/Example_scripts/test_r_script.r
@@ -0,0 +1,10 @@
+
+# A comment: this is a sample script.
+
+y=c(12,15,28,17,18)
+x=c(22,39,50,25,18)
+mean(y)
+mean(x)
+
+# note that the r plot is saved to your working directory
+plot(x,y)
