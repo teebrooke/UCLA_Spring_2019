20e40a2b7792bca605b44cb6daa3d6b4f397584d
limey-bean <eecurd@g.ucla.edu>
2019-05-11 13:00:41 -0700

    Update test_r_script.r

--- Term_project/Example_scripts/test_r_script.r
+++ Term_project/Example_scripts/test_r_script.r
@@ -1,10 +1,11 @@
 
 # A comment: this is a sample script.
-
+# y and x are vectors that contain numeric strings.
 y=c(12,15,28,17,18)
 x=c(22,39,50,25,18)
+# take the mean of x and y
 mean(y)
 mean(x)
 
-# note that the r plot is saved to your working directory
+# note that the r plot is saved to your working directory, it is an x y scatter plot
 plot(x,y)
